b2be6ce7d5cae6d075a8c583cfcaa698458e8704
Muhammad Talha <talhaguy@gmail.com>
2022-07-16 21:48:26 -0700

    Move class outside of mixin function call

--- src/authentication/jwt-authentication-controller.ts
+++ src/authentication/jwt-authentication-controller.ts
@@ -16,85 +16,86 @@ export interface LoginRequestPayload {
   password: string;
 }
 
-export const JWTAuthenticationController = JWTTokenResponder(
-  class JWTAuthenticationController {
-    constructor(
-      public _accessJWTTokenManager: JWTTokenManager<JWTTokenPayload>,
-      public _refreshTokenManager: JWTTokenManager<JWTTokenPayload>,
-      public _userRepo: UserRepo,
-      public _passwordHasher: PasswordHasher,
-      public _errorHandler: ErrorHandler
-    ) {}
+class _JWTAuthenticationController {
+  constructor(
+    public _accessJWTTokenManager: JWTTokenManager<JWTTokenPayload>,
+    public _refreshTokenManager: JWTTokenManager<JWTTokenPayload>,
+    public _userRepo: UserRepo,
+    public _passwordHasher: PasswordHasher,
+    public _errorHandler: ErrorHandler
+  ) {}
 
-    get accessJWTTokenManager() {
-      return this._accessJWTTokenManager;
-    }
+  get accessJWTTokenManager() {
+    return this._accessJWTTokenManager;
+  }
 
-    get refreshTokenManager() {
-      return this._refreshTokenManager;
-    }
+  get refreshTokenManager() {
+    return this._refreshTokenManager;
+  }
 
-    get errorHandler() {
-      return this._errorHandler;
-    }
+  get errorHandler() {
+    return this._errorHandler;
+  }
 
-    public async login(req: Request, res: Response) {
-      const { username, password } = req.body;
+  public async login(req: Request, res: Response) {
+    const { username, password } = req.body;
 
-      let storedUser: User | null;
-      try {
-        storedUser = await this._userRepo.getUser(username);
-        if (!storedUser) {
-          this._errorHandler(res, AuthenticationErrorType.NoExistingUser);
-          return;
-        }
-      } catch (err) {
-        this._errorHandler(
-          res,
-          AuthenticationErrorType.ErrorGettingUser,
-          err as Error
-        );
+    let storedUser: User | null;
+    try {
+      storedUser = await this._userRepo.getUser(username);
+      if (!storedUser) {
+        this._errorHandler(res, AuthenticationErrorType.NoExistingUser);
         return;
       }
-      const doesPasswordMatch = await this._passwordHasher.compare(
-        password,
-        storedUser.password
+    } catch (err) {
+      this._errorHandler(
+        res,
+        AuthenticationErrorType.ErrorGettingUser,
+        err as Error
       );
-      if (!doesPasswordMatch) {
-        this._errorHandler(res, AuthenticationErrorType.PaswordMismatch);
-        return;
-      }
-
-      this.respondWithTokens(res, username);
-    }
-
-    public async refresh(req: Request, res: Response) {
-      const refreshToken = req.cookies[REFRESH_TOKEN_COOKIE_NAME];
-      if (!refreshToken) {
-        this._errorHandler(res, AuthenticationErrorType.NoRefreshToken);
-        return;
-      }
-
-      let username: string;
-      try {
-        const tokenPayload = await this._refreshTokenManager.verify(
-          refreshToken
-        );
-        username = tokenPayload["username"];
-      } catch (err) {
-        this._errorHandler(
-          res,
-          AuthenticationErrorType.InvalidRefreshToken,
-          err as Error
-        );
-        return;
-      }
-
-      this.respondWithTokens(res, username);
-    }
-
-    public async respondWithTokens(_: Response, __: string) {
       return;
     }
+    const doesPasswordMatch = await this._passwordHasher.compare(
+      password,
+      storedUser.password
+    );
+    if (!doesPasswordMatch) {
+      this._errorHandler(res, AuthenticationErrorType.PaswordMismatch);
+      return;
+    }
+
+    this.respondWithTokens(res, username);
   }
+
+  public async refresh(req: Request, res: Response) {
+    const refreshToken = req.cookies[REFRESH_TOKEN_COOKIE_NAME];
+    if (!refreshToken) {
+      this._errorHandler(res, AuthenticationErrorType.NoRefreshToken);
+      return;
+    }
+
+    let username: string;
+    try {
+      const tokenPayload = await this._refreshTokenManager.verify(refreshToken);
+      username = tokenPayload["username"];
+    } catch (err) {
+      this._errorHandler(
+        res,
+        AuthenticationErrorType.InvalidRefreshToken,
+        err as Error
+      );
+      return;
+    }
+
+    this.respondWithTokens(res, username);
+  }
+
+  // to be overridden by subclass
+  public async respondWithTokens(_: Response, __: string) {
+    return;
+  }
+}
+
+export const JWTAuthenticationController = JWTTokenResponder(
+  _JWTAuthenticationController
 );
