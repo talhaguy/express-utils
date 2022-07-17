4311dad9db74250f25b951efce14f97e9a5a3c14
Muhammad Talha <talhaguy@gmail.com>
2022-07-16 23:04:38 -0700

    Alternative dist strategy for publish

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./application";
-export * from "./authentication";
-export * from "./headers";
-export * from "./models";
-export * from "./schema-validation";
-export * from "./utils";
